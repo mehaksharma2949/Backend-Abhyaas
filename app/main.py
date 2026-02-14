38e49d285143026b5ebf7a1b9b3602aeb672b872
mehaksharma2949 <147977378+mehaksharma2949@users.noreply.github.com>
2026-02-14 16:37:59 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from app.routers import (
     auth, otp, dashboard,
     worksheet, diagram, fluency,
     story, teachback, lesson_plan,
-    video_generation, answer_sheet_evaluator
+    video_generation, answer_sheet_evaluator,doubt_solver
 )
 
 app = FastAPI(title="Abhyaas Backend Full")
@@ -41,3 +41,5 @@ app.include_router(teachback.router, prefix="/teachback")
 app.include_router(lesson_plan.router, prefix="/lesson-plan")
 app.include_router(video_generation.router, prefix="/video")
 app.include_router(answer_sheet_evaluator.router, prefix="/answer-sheet")
+app.include_router(doubt_solver.router, prefix="/doubt")
+
